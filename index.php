7a64bacc1d69aa3f2c4f7ebd5f141c2b737bf782
Manuz <168524071+ManuelZuccolo@users.noreply.github.com>
2026-02-18 14:19:36 +0100

    Aggiunta tutti i tipi per filtri

--- index.php
+++ index.php
@@ -120,20 +120,43 @@ $result = $conn->query($sql);*/
         <option value="water">Water</option>
         <option value="grass">Grass</option>
         <option value="electric">Electric</option>
+        <option value="ice">Ice</option>
+        <option value="fighting">Fighting</option>
+        <option value="poison">Poison</option>
+        <option value="ground">Ground</option>
+        <option value="flying">Flying</option>
+        <option value="psychic">Psychic</option>
+        <option value="bug">Bug</option>
+        <option value="rock">Rock</option>
         <option value="ghost">Ghost</option>
         <option value="dragon">Dragon</option>
-        <!-- aggiungi gli altri -->
+        <option value="dark">Dark</option>
+        <option value="steel">Steel</option>
+        <option value="fairy">Fairy</option>
+        <option value="normal">Normal</option>
     </select>
 
     <!-- Tipo 2 -->
     <select name="type2">
-        <option value="">Tipo 2</option>
+        <option value="">Tipo 1</option>
         <option value="fire">Fire</option>
         <option value="water">Water</option>
         <option value="grass">Grass</option>
         <option value="electric">Electric</option>
+        <option value="ice">Ice</option>
+        <option value="fighting">Fighting</option>
+        <option value="poison">Poison</option>
+        <option value="ground">Ground</option>
+        <option value="flying">Flying</option>
+        <option value="psychic">Psychic</option>
+        <option value="bug">Bug</option>
+        <option value="rock">Rock</option>
         <option value="ghost">Ghost</option>
         <option value="dragon">Dragon</option>
+        <option value="dark">Dark</option>
+        <option value="steel">Steel</option>
+        <option value="fairy">Fairy</option>
+        <option value="normal">Normal</option>
     </select>
 
     <!-- Range codice -->
